902fa0501704d221fe65cd19930b2b344606117e
Mohamed Jabri <mohamedjabri900@gmail.com>
2026-07-30 21:47:30 +0200

    new updates

--- gv_start_project/scenes/levels/level.gd
+++ gv_start_project/scenes/levels/level.gd
@@ -4,8 +4,12 @@ extends Node2D
 @onready var grass_layer: TileMapLayer = $Layers/GrassLayer
 @onready var player: CharacterBody2D = $Objects/Player
 
+var plant_scene = preload("res://scenes/objects/plant.tscn")
+var used_cells: Array[Vector2i]
+
 func _on_player_tool_use(tool: Enum.Tool, pos: Vector2) -> void:
 	var grid_coord: Vector2i = soil_layer.local_to_map(soil_layer.to_local(pos))
+	var has_soil: bool = grid_coord in soil_layer.get_used_cells()
 	
 	match tool:
 		Enum.Tool.HOE:
@@ -14,13 +18,18 @@ func _on_player_tool_use(tool: Enum.Tool, pos: Vector2) -> void:
 				soil_layer.set_cells_terrain_connect([grid_coord], 0, 0)
 			
 		Enum.Tool.WATER:
-			var cell = soil_layer.get_cell_tile_data(grid_coord) as TileData
-			if cell:
+			if has_soil:
 				water_soil_layer.set_cell(grid_coord, 0, Vector2i(randi_range(0,2), 0))
 		
 		Enum.Tool.FISH:
 			if not grid_coord in grass_layer.get_used_cells():
 				print("fishing")
+		
+		Enum.Tool.SEED:
+			if has_soil and grid_coord not in used_cells:
+				var plant = plant_scene.instantiate()
+				plant.setup(grid_coord, $Objects)
+				used_cells.append(grid_coord)
 				
 				
 			
